6e45f7adac3430302ffe483d89e38d09448db635
Uladin <Ulascan.Yildirim@hotmail.com>
2024-06-17 10:28:04 +0200

    validate password length

--- Secretify/Functions/Get-RandomPassword.ps1
+++ Secretify/Functions/Get-RandomPassword.ps1
@@ -46,6 +46,11 @@ function Get-RandomPassword {
         [bool]$IncludeUppercase = $true
     )
 
+    # Validate Length parameter
+    if ($Length -lt 5) {
+        throw "Length must be at least 5 and cannot be negative."
+    }
+
     # Define character sets as arrays
     $numbers = @('0','1','2','3','4','5','6','7','8','9')
     $symbols = @('!','@','#','$','^','*','(',')','_','+','-','=','[',']','{','}',';',':',',','.','?')
